71cb8b67fd935e31af163c2140ad9df32d3c4106
= <mihai@academicmerit.com>
2021-07-19 20:45:22 +0300

    Initial commit

---
+++ python-hello-world/main.py
@@ -0,0 +1,45 @@
+# this is a comment
+# print("Hello, world")  # this is an inline comment
+# print('My age is', 20, 'years old')
+# print(20, type(20), type(20).__name__)
+# print(21.50, type(21.50), type(21.50).__name__)
+# print(3 + 5j, type(3 + 5j), type(3 + 5j).__name__)
+# print(float(20), type(float(20)), type(float(20)).__name__)
+# print(complex(20), type(complex(20)), type(complex(20)).__name__)
+# print(int(20.50), type(int(20.50)), type(int(20.50)).__name__)
+# print(complex(20.50), type(complex(20.50)), type(complex(20.50)).__name__)
+# print('5/2 are catul =', 5//2, 'si restul = ', 5 % 2); print(5/2)
+# print(5 + 2 / 3 - 27 + 5.60)
+# None, True, False
+# print(7 is None)
+# my_age = 20
+# print(my_age, type(my_age))
+# my_age = 'hello, world!'
+# print(my_age, type(my_age))
+# nr_1 = 5
+# nr_2 = 7
+# # print('sum of numbers', nr_1 + nr_2)
+# print('nr_1 is at address:', id(nr_1))
+# print('nr_2 is at address:', id(nr_2))
+# my_var = 5
+# print(my_var)
+# my_var += 3  # my_var = my_var + 3
+# print(my_var)
+# print('This is the first line\n\nThis is the second line.')
+# print('''This is the first line
+# This is the second line
+#
+# This is the third line''')
+# my_string = 'Litera mea preferata este \'a\''
+# print('my_string', my_string)
+
+# print('This first line\\nThis is second line')
+# print(r'This first line\nThis is second line')
+
+# my_age = 20  # str(my_age) = '20'
+# my_name = 'Mihai'
+# my_string = 'My age is: ' + str(my_age) + '.'
+# my_string = 'Hello {0}! My name is: {name} and my age is: {age}.'.format('Everyone', name=my_name, age=my_age)
+# my_string = 'Hello %s! My name is: %s and my age is %d.' % ('Everyone', my_name, my_age)
+# my_string = f'Hello {"Everyone"}! My name is: {my_name} and my age is: {my_age}.'
+# print(my_string)
