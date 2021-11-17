97f5fabaf1a6da3d745b668502ca153f3b599a6a
mihai <mihai@academicmerit.com>
2021-11-17 19:58:19 +0200

    Update BASE_DIR settings.

--- web-ecommerce-app/ecommerce/settings/base.py
+++ web-ecommerce-app/ecommerce/settings/base.py
@@ -16,7 +16,6 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-print('BASE_DIR', BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
